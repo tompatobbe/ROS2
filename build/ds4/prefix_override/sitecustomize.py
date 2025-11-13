013c63f503fa5ddbba6d42d4d297503ec510ef12
Tobias Thorgren <thorgrentobias@gmail.com>
2025-11-13 12:59:37 +0100

    hello

--- build/ds4/prefix_override/sitecustomize.py
+++ build/ds4/prefix_override/sitecustomize.py
@@ -1,4 +1,4 @@
 import sys
 if sys.prefix == '/usr':
     sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/tobbe/ros2_ws/install/ds4'
+    sys.prefix = sys.exec_prefix = '/home/tobbe/ROS2/install/ds4'
